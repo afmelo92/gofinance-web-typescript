a9ae06ae1ba1aa181d3558e8d6c88d43e37dd3ad
Andre Melo <andre.fabian.melo@gmail.com>
2020-05-07 16:45:46 -0300

    Add header links and routing

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,9 +15,8 @@ const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
     <header>
       <img src={logoImg} alt="GoFinances" />
       <nav>
-        {
-          // Todo
-        }
+        <Link to="/">Listagem</Link>
+        <Link to="/import">Importar</Link>
       </nav>
     </header>
   </Container>
